077335647a5368e4497c5abb39b0f3e8babb2c40
riyad5541 <abdullahalriyad5541@gmail.com>
2022-11-10 22:27:25 +0600

    add review in details page

--- src/Pages/DetailsPage/DetailsPage.js
+++ src/Pages/DetailsPage/DetailsPage.js
@@ -4,7 +4,40 @@ import { useLoaderData } from 'react-router-dom';
 const DetailsPage = () => {
     const allDetails = useLoaderData();
     const {title, img, price, description} = allDetails
+
+    const handleAddReview = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const ImageURL = form.ImageURL.value;
+        const review = form.review.value;
+
+        const order ={
+            name,
+            ImageURL,
+            review
+        }
+
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST' ,
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.acknowledged){
+                alert('Review Add Sucessfully')
+                form.reset();
+            }
+        
+        })
+        .catch(er => console.error(er));
+    }
     return (
+       <>
         <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto my-12">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
@@ -16,6 +49,19 @@ const DetailsPage = () => {
                 </div>
             </div>
         </div>
+        <div>
+        <h1 className='text-5xl text-center my-12 text-orange-800' >Review Section</h1>
+            <form onSubmit={handleAddReview}>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-12'>
+                    <input name='name' type="text" placeholder="Your name" className="input input-bordered w-full" required/>
+                    <input name='ImageURL' type="text" placeholder="ImageURL" className="input input-bordered w-full" required />
+                    <textarea name='review' className="textarea textarea-success w-full" placeholder="Give Review" required></textarea>
+                </div>
+                <input className='btn mb-12' type='submit' value='Add Review' />
+
+            </form>
+        </div>
+       </>
     );
 };
 
--- src/Pages/Home/Home/MyReview/MyReview.js
+++ src/Pages/Home/Home/MyReview/MyReview.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const MyReview = () => {
+
+    
     return (
         <div>
-            this is my review
+            
         </div>
     );
 };
